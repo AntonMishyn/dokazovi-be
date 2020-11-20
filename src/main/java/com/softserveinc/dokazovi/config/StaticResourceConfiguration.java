82db433f3d97b2d29f937c87b307b15b9233707a
brmdm <60578980+brmdm@users.noreply.github.com>
2020-11-20 18:40:36 +0200

    minor refactor

---
+++ src/main/java/com/softserveinc/dokazovi/config/StaticResourceConfiguration.java
@@ -0,0 +1,39 @@
+package com.softserveinc.dokazovi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+import java.nio.file.Path;
+
+@Configuration
+public class StaticResourceConfiguration implements WebMvcConfigurer {
+
+	@Value("${fs.root}")
+	private String fsPath;
+
+	@Value("${url.assets.path}")
+	private String urlAssetsPath;
+
+	private final long MAX_AGE_SECS = 3600;
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+				.addResourceHandler("/" + urlAssetsPath + "/**")
+				.addResourceLocations("file:///" + Path.of(fsPath).toAbsolutePath() + File.separator);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+				.allowedHeaders("*")
+				.allowCredentials(true)
+				.maxAge(MAX_AGE_SECS);
+	}
+}
--- src/main/java/com/softserveinc/dokazovi/exception/ResourceNotFoundException.java
+++ src/main/java/com/softserveinc/dokazovi/exception/ResourceNotFoundException.java
@@ -1,9 +1,11 @@
 package com.softserveinc.dokazovi.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
+@Getter
 public class ResourceNotFoundException extends RuntimeException {
     private String resourceName;
     private String fieldName;
